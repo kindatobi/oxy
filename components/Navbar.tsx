d0276d3d2dbe904d783c1ec344050e523c7ceff4
blur <thetobiterra@gmail.com>
2026-02-11 16:37:27 +0100

    nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,29 @@
+"use client";
+
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { Moon } from "@phosphor-icons/react/dist/ssr";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes";
+import {
+  GearIcon,
+  MoonIcon,
+  SignOutIcon,
+  SunIcon,
+  UserIcon,
+} from "@phosphor-icons/react";
 
 export default function Navbar() {
+  const { setTheme } = useTheme();
   return (
     <nav className="flex p-4 items-center justify-between">
       {/* LEFT */}
@@ -10,11 +31,53 @@ export default function Navbar() {
       {/* RIGHT */}
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
-        <Moon size={32} />
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon" className="relative">
+              <SunIcon size={18} className="absolute scale-100 dark:scale-0" />
+              <MoonIcon size={18} className="absolute scale-0 dark:scale-100" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-40" align="start">
+            <DropdownMenuGroup>
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuGroup>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuItem>
+                <UserIcon size={32} />
+                <span className="ml-2">Profile</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <GearIcon size={32} />
+                <span className="ml-2">Settings</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem variant="destructive">
+                <SignOutIcon size={32} />
+                <span className="ml-2">Logout</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
