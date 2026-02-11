4c682acbbff6f59b68d02d980f35b431070abd63
blur <thetobiterra@gmail.com>
2026-02-11 09:37:35 +0100

    phosphor

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Moon } from "@phosphor-icons/react/dist/ssr";
+
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <nav className="flex p-4 items-center justify-between">
+      {/* LEFT */}
+      collapse btn
+      {/* RIGHT */}
+      <div className="flex items-center gap-4">
+        <Link href="/">Dashboard</Link>
+        <Moon size={32} />
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </nav>
+  );
 }
